b17f3e48a952dbf2bb8a2ed3cb24585bbb36af1e
anureshmp <anureshmp@gmail.com>
2024-06-27 13:40:34 +1000

    Initial Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController:ControllerBase
+{
+
+private readonly DataContext _context;
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+
+        return users;
+
+    }
+
+    [HttpGet("{Id:int}")]
+    public async Task<ActionResult<AppUser>> GetUser(int Id)
+    {
+        var user = await  _context.Users.FindAsync(Id);
+        if (user == null) return NotFound();
+
+        return user;
+
+    }
+
+
+}
